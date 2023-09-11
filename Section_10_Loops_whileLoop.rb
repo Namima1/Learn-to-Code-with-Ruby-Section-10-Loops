38a4270b284504e04baea3a3c90b690b9ace5f78
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-11 12:34:39 -0400

    Add files via upload

---
+++ Section_10_Loops_whileLoop.rb
@@ -0,0 +1,14 @@
+# A loop is a series of instructions that is repeated 
+# it gets repeated until a terminating condition is reached
+
+3.times { print "Hello" }
+#times method is a loop, it is a series of instructions and it's repeating 
+
+count = 1 
+
+while count < 10 
+  puts count
+  count += 1
+end
+
+#count += 1 is taking the count and goes up by one, this will stop at 9  
